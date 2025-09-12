0a432139715dbd89d8b0415ad7c48154c5159cf0
dariodev1 <dariuszszpunar@outlook.com>
2025-09-12 15:00:30 +0200

    toaster library installed and configured

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 
 
@@ -11,7 +12,7 @@ import { AccountService } from '../_services/account.service';
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
-  constructor(private accountService: AccountService) { }
+  constructor(private accountService: AccountService,private toaster: ToastrService) { }
 
   ngOnInit(): void {
   }
@@ -22,6 +23,7 @@ export class RegisterComponent implements OnInit {
       this.cancel();
     },error=>{
       console.log(error);
+      this.toaster.error(error.error);
     })
   }
 
