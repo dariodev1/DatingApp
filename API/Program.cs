33053961566cd6bc295799b09f2a81d378e8f749
dariodev1 <dariuszszpunar@outlook.com>
2025-09-15 17:35:50 +0200

    seed test data to db

--- API/Program.cs
+++ API/Program.cs
@@ -30,7 +30,20 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJw
     };
 });
 var app = builder.Build();
-
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}
+catch (Exception ex)
+{
+    var logger = app.Services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured durin migration");
+}
+await app.RunAsync();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
