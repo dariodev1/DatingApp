8c22aad7ae744f433cb232344b042f127b3c51b8
dariodev1 <dariuszszpunar@outlook.com>
2025-09-15 22:01:51 +0200

    fixed strange db error

--- API/Data/Migrations/20250915195450_MakePublicIdNullable.Designer.cs
+++ API/Data/Migrations/20250915195450_MakePublicIdNullable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace API.Data.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20250915195450_MakePublicIdNullable")]
+    partial class MakePublicIdNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.8");
@@ -90,7 +93,6 @@ namespace API.Data.Migrations
                         .HasColumnType("INTEGER");
 
                     b.Property<string>("PublicId")
-                        .IsRequired()
                         .HasColumnType("TEXT");
 
                     b.Property<string>("Url")
--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -17,7 +17,7 @@ namespace API.Data
         }
         public async Task<AppUser> GetByUsernameAsync(string username)
         {
-            return await _context.Users.SingleOrDefaultAsync(x=>x.UserName == username);
+            return await _context.Users.Include(p => p.Photos).SingleOrDefaultAsync(x => x.UserName == username);
         }
 
         public async Task<AppUser> GetUserByIdAsync(int id)
@@ -27,7 +27,7 @@ namespace API.Data
 
         public async Task<IEnumerable<AppUser>> GetUsersAsync()
         {
-            return await _context.Users.ToListAsync();
+            return await _context.Users.Include(p=>p.Photos).ToListAsync();
         }
 
         public async Task<bool> SaveAllAsync()
