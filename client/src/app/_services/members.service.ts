31e44e387a7c0e939bd7c0c4fd99cca52f58ed77
dariodev1 <dariuszszpunar@outlook.com>
2025-10-06 21:16:14 +0200

    remembering the filters for a user in the service

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -3,9 +3,11 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
 import { of } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, take } from 'rxjs/operators';
 import { PaginatedResult } from '../_models/pagination';
 import { UserParams } from '../_models/userParams';
+import { AccountService } from './account.service';
+import { User } from '../_models/user';
 
 
 @Injectable({
@@ -15,9 +17,28 @@ export class MembersService {
   baseUrl = environment.apiUrl;
   members: Member[] = [];
   memberCache = new Map();
+  user: User;
+  userParams: UserParams;
 
+  constructor(private http: HttpClient, private accountService: AccountService) { 
+    this.accountService.currentUser$.pipe(take(1)).subscribe(user=>{
+          this.user = user;
+          this.userParams = new UserParams(user);
+        })
+  }
 
-  constructor(private http: HttpClient) { }
+  getUserParams(){
+    return this.userParams;
+  }
+
+  setUserParams(params: UserParams){
+    this.userParams = params;
+  }
+
+  resetUserParams(){
+    this.userParams = new UserParams(this.user);
+    return this.userParams;
+  }
 
   getMembers(userParams: UserParams) {
     var response = this.memberCache.get(Object.values(userParams).join('-'));
@@ -43,8 +64,15 @@ export class MembersService {
 
 
   getMember(username) {
-    const member = this.members.find(x => x.username === username);
-    if (member !== undefined) return of(member);
+    const member = [...this.memberCache.values()]
+    .reduce((arr,elem)=> arr.concat(elem.result),[])
+    .find((member: Member) =>member.username === username);
+    
+    if(member)
+    {
+      return of(member);
+    }
+
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 
