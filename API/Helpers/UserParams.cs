88b8a3919d090475fdee7a7d8c04383251d1f9af
dariodev1 <dariuszszpunar@outlook.com>
2025-10-06 13:39:15 +0200

    sorting functionality added

--- API/Helpers/UserParams.cs
+++ API/Helpers/UserParams.cs
@@ -22,6 +22,7 @@ namespace API.Helpers
         public string? Gender { get; set; }
         public int MinAge { get; set; } = 18;
         public int MaxAge { get; set; } = 150;
+        public string OrderBy { get; set; } = "lastActive";
 
 
     }
