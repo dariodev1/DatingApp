a00db97504169547d55357a0e51bad150a00b91f
dariodev1 <dariuszszpunar@outlook.com>
2025-10-12 22:20:27 +0200

    adding roles to the token

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -25,6 +25,7 @@ namespace API.Controllers
             _photoService = photoService;
         }
 
+        
         [HttpGet]
         public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers([FromQuery] UserParams userParams)
         {
@@ -42,6 +43,7 @@ namespace API.Controllers
             return Ok(users);
         }
 
+        
         [HttpGet("{username}", Name = "GetUser")]
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
